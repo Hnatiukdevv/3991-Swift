d7d1403e8504f2c6cd5fe80814f41335c843ec36
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-26 10:35:12 +0200

    init

---
+++ Pods/Target Support Files/Pods-TeamTurnly/Pods-TeamTurnly-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TeamTurnly : NSObject
+@end
+@implementation PodsDummy_Pods_TeamTurnly
+@end
---
+++ TeamTurnly/App/TeamTurnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamTurnlyApp.swift
+//  TeamTurnly
+//
+//  Created by Serhii Babchuk on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TeamTurnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TTLaunchView()
+        }
+    }
+}
