f5aceadaf84721d0b3f6330a33e5d1368201f90e
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-09 11:25:45 +0200

    add grey part app

--- TeamTurnly/App/TeamTurnlyApp.swift
+++ TeamTurnly/App/TeamTurnlyApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct TeamTurnlyApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            TTLaunchView()
+            AppEntryPoint()
         }
     }
 }
